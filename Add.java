999d6b94acd0727d56b3a56f30c947bfc2990c5f
Kasturi1805 <swapnagokul8@gmail.com>
2025-04-22 09:29:25 -0500

    save

---
+++ Add.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Add {
+
+    public static void main(String[] args)
+
+    {
+        int a = 5, b = 6;
+        System.out.println("Main");
+        System.out.println("Additon" +(a+b)); 
+    }
+
+    
+}
